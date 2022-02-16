557cd32bb0b08ccca94edfcbb83f83d863988c32
Carson Richter <carson.ri@protonmail.com>
2022-02-15 21:03:08 -0700

    new item styling

--- app/routes/item/$itemId.tsx
+++ app/routes/item/$itemId.tsx
@@ -16,7 +16,6 @@ export default function ItemPage() {
             <h1 className="text-3xl">Item Page</h1>
             <h3 className="text-2xl">{uniqueItem.name}</h3>
             { uniqueItem.box.name ? <p>Box: {uniqueItem.box.name}</p> : <p>Box: To be packed</p> }
-            
         </div>
     )
 }
--- app/routes/item/new.tsx
+++ app/routes/item/new.tsx
@@ -43,8 +43,6 @@ export default function NewItem() {
                         {boxes.map((box) => (
                             <option value={box.id}>{box.name}</option>
                         ))}
-                        
-                        
                     </select>
                 </div>
                 <button type="submit" className="bg-slate-200 py-2 px-6 rounded hover:bg-slate-400">
