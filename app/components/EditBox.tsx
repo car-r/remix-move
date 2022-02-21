c87a1c41dc426a4e9a90be53c75419d21f8e73a6
Carson Richter <carson.ri@protonmail.com>
2022-02-20 23:48:29 -0700

    validation on Edit box

---
+++ app/components/EditBox.tsx
@@ -0,0 +1,52 @@
+import { useActionData } from "remix"
+import { action } from "~/routes/box/$boxId"
+
+
+export default function EditBox({ uniqueBox }) {
+    const actionData = useActionData()
+    return (
+        <div className="mb-4 flex border border-slate-200 p-4 rounded">
+            <form method="post" className="w-full">
+                <div className="flex flex-col mb-4">
+                    <label className="mb-2">Box Name: </label>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        defaultValue={uniqueBox.name} 
+                        className="border border-slate-200 rounded px-2 py-1"
+                    />
+                    {actionData?.errors.name && (
+                        <p className="text-red-400">{actionData.errors.name}</p>
+                    ) }
+                </div>
+                <div className="flex flex-col mb-4">
+                    <label className="mb-2">Room Name: </label>
+                    <input 
+                        type="text" 
+                        name="room" 
+                        defaultValue={uniqueBox.room} 
+                        className="border border-slate-200 rounded px-2 py-1"/>
+                        {actionData?.errors.room && (
+                        <p className="text-red-400">{actionData.errors.room}</p>
+                    ) }
+                </div>
+                <div className="flex flex-col mb-4">
+                    <label className="mb-2">Box Size: </label>
+                    <select name="size" className="border border-slate-200 rounded px-2 py-1">
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                    </select>
+                </div>
+                <div className="flex justify-between">
+                    <button type="submit" name="_method" value="update" className="bg-blue-500/75 text-white py-2 px-6 rounded hover:bg-blue-700/75 transition-all duration-300">
+                        Save Box
+                    </button>
+                    <button type="submit" name="_method" value="delete" className="bg-red-400 text-white py-2 px-6 rounded hover:bg-red-600 transition-all duration-300">
+                        Delete
+                    </button>
+                </div>   
+            </form>
+        </div>
+    )
+}
--- app/routes/box/$boxId/index.tsx
+++ app/routes/box/$boxId/index.tsx
@@ -1,4 +1,4 @@
-import { ActionFunction, LoaderFunction, redirect, useActionData, useLoaderData } from "remix"
+import { LoaderFunction, useLoaderData} from "remix"
 import { db } from "~/utils/db.server"
 
 export const loader: LoaderFunction = async ({ params }) => {
@@ -8,34 +8,20 @@ export const loader: LoaderFunction = async ({ params }) => {
     return { ...box, items}
 }
 
-
-export const action: ActionFunction = async ({ request }) => {
-    const form = await request.formData()
-    const name = form.get('name')
-    const boxId = form.get('boxId')
-    if (
-        typeof name !== 'string' ||
-        typeof boxId !== 'string'
-    ) {
-        throw new Error('Form not submitted correctly.')
-    }
-
-    const fields = { name, boxId }
-    const item = await db.item.create( {data: fields })
-    return redirect(`/box/${boxId}`)
-    
-}
-
 export default function BoxIdOutlet() {
     const box = useLoaderData()
-    
+
     return (
         <div className="flex border border-slate-200 p-4 rounded mb-4">
-            
             <form method="post">
                 <div className="flex flex-col mb-4">
                     <label className="mb-2" >Name: </label>
-                    <input type="text" name="name" placeholder="Enter Item Name" className="border border-slate-200 rounded px-2 py-1"/>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Enter Item Name" 
+                        className="border border-slate-200 rounded px-2 py-1"
+                    />
                 </div>
                 <div className="flex flex-col mb-4">
                     <label className="mb-2">Box: </label>
@@ -43,7 +29,7 @@ export default function BoxIdOutlet() {
                             <option value={box.id}>{box.name}</option>
                     </select>
                 </div>
-                <button type="submit" name="_method" value="create" className="bg-slate-400 border border-slate-400/75 bg-opacity-75 text-white py-2 px-6 rounded hover:bg-slate-200 hover:text-black hover:border hover:border-slate-400 transition-all ease-in-out duration-300">
+                <button type="submit" name="_method" value="create" className="bg-blue-500/75 text-white py-2 px-6 rounded hover:bg-blue-700/75 transition-all duration-300">
                     Add Item
                 </button>
             </form>
