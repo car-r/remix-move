3997deab8fd671d7a9cc4c7713f82022c8e5e563
Carson Richter <carson.ri@protonmail.com>
2022-03-08 20:48:28 -0700

    class to className on navbar svg

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -11,14 +11,14 @@ export default function NavBar() {
             </Link>
 
             <Link to='/room' className="flex flex-col items-center cursor-pointer hover:text-indigo-500">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
                 <p className="text-sm">Rooms</p>
             </Link>
             
             <Link to='/dashboard' className="flex flex-col items-center cursor-pointer hover:text-indigo-500">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
                 </svg>
                 <p className="text-sm ">Dashboard</p>
