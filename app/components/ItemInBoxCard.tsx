f140bce807261869b569267a6a5c492867c2f482
Carson Richter <carson.ri@protonmail.com>
2022-03-11 23:02:28 -0700

    implemented grid layout in box route

--- app/components/ItemInBoxCard.tsx
+++ app/components/ItemInBoxCard.tsx
@@ -18,7 +18,7 @@ export type Item = {
 
 export default function ItemInBoxCard({item}: Item) {
     return (
-        <div className="flex flex-col rounded-md p-4 bg-white lg:w-1/2">
+        <div className="flex flex-col rounded-md p-4 bg-white h-full">
             <h3 className="text-2xl font-bold">{item.name}</h3>
             <p className="font-semibold mb-6">
                 Last updated: <span className="font-thin">{moment(item.updatedAt).format('MMM DD, YYYY')}</span>
